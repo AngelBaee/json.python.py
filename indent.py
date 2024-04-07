4a677f453d97e82bd2303dd2c9debaceae89a962
Emily <140002969+AngelBaee@users.noreply.github.com>
2024-04-07 18:22:19 +0500

    Add files via upload

---
+++ indent.py
@@ -0,0 +1,17 @@
+import json
+
+lol = {
+    "name": "Sam",
+    "age": "35",
+    "married": True,
+    "divorced": False,
+    "children":("lily","Dan"),
+    "pets": None,
+    "cars": [
+        {"model": "BMW M5", "mpg":27.5},
+        {"model": "Ford MUstang", "mpg": 28.1}
+    ]
+
+}
+
+print(json.dumps(lol, indent=4))
